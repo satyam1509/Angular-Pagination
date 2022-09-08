a2499ee4478253905ca563777afd05670a9156ec
satyam1509 <patel1996satyam@gmail.com>
2022-09-08 16:51:09 +0530

    pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'pagination';
+  page = 1;
+  data:Observable<any>;
+even: any;
+odd: any;
+  constructor(private httpclient: HttpClient) {
+  this.data = this.httpclient.get('https://jsonplaceholder.typicode.com/posts');}
+
 }
